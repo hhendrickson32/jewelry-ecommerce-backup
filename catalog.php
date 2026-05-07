20ad1797c4489b64d1fb84825654404d9e4916d5
hhendrickson32 <hhendrickson@mtech.edu>
2026-05-07 09:32:34 -0600

    Add alt attributes to jewelry images
    Added alt attributes to images for better accessibility.

--- catalog.php
+++ catalog.php
@@ -3,17 +3,17 @@
 <h2>Our Jewelry Collection</h2>
 
 <div>
-    <img src="images/necklace.jpg" width="200">
+    <img src="images/necklace.jpg" width="200" alt="Gold Necklace">
     <p>Gold Necklace - $120</p>
 </div>
 
 <div>
-    <img src="images/ring.jpg" width="200">
+    <img src="images/ring.jpg" width="200" alt="Diamond Ring">
     <p>Diamond Ring - $450</p>
 </div>
 
 <div>
-    <img src="images/bracelet.jpg" width="200">
+    <img src="images/bracelet.jpg" width="200" alt="Silver Bracelet">
     <p>Silver Bracelet - $85</p>
 </div>
 
