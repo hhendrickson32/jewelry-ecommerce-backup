de9284d4e1eef0c87863d6552a4ae45e6dc5122f
hhendrickson32 <hhendrickson@mtech.edu>
2026-05-07 09:35:59 -0600

    Add language and meta tags to header.php

--- header.php
+++ header.php
@@ -1,9 +1,12 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hillary's Jewelry Boutique</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
 
 <header>
@@ -20,4 +23,5 @@
         </ul>
     </nav>
 </header>
+
 <hr>
