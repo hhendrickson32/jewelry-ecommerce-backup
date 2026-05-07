2a1752ca81818d9ea020f1e477352d7a92726727
hhendrickson32 <hhendrickson@mtech.edu>
2026-05-07 09:34:00 -0600

    Update and rename Checkout.php to checkout.php

--- checkout.php
+++ checkout.php
@@ -3,16 +3,18 @@
 <h2>Checkout</h2>
 
 <form action="order-confirmation.php" method="post">
+
     <label>Full Name:</label>
-    <input type="text" name="fullname"><br><br>
+    <input type="text" name="fullname" required><br><br>
 
     <label>Shipping Address:</label>
-    <input type="text" name="address"><br><br>
+    <input type="text" name="address" required><br><br>
 
     <label>Credit Card Number:</label>
-    <input type="text" name="cardnumber"><br><br>
+    <input type="password" name="cardnumber" required><br><br>
 
     <input type="submit" value="Place Order">
+
 </form>
 
 <?php include 'footer.php'; ?>
